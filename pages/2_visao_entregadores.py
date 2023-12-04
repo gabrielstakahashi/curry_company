914781b03045361c8d46603dbf84be59ef7b9c45
Gabriel <97264006+gabrielstakahashi@users.noreply.github.com>
2023-12-04 12:13:23 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -148,7 +148,7 @@ df1 = df1.loc[linhas_selecionadas, :]
 #           layout no streamlit
 # ========================================
 
-tab1 = st.tabs( 'Visão Gerencial' )
+tab1 = st.tab( 'Visão Gerencial' )
 
 with tab1:
     with st.container():
